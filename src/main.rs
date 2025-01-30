c6b7c43a323f79c75e855374b6ce1dd3528de10e
Xharry01X <harshitsingh50621@gmail.com>
2025-01-30 23:14:49 +0530

    add comments to learn better understanding.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // Import required traits and structs from actix_web
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
+// impl Responder means this function returns a type that can be converted to an HTTP response
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -9,6 +11,7 @@ async fn greet(req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+     // Create a new HTTP server instance
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
