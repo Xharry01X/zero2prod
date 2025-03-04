9ae7e5271118deeb712b2697c45665be77999533
Xharry01X <harshitsingh50621@gmail.com>
2025-03-04 11:39:06 +0530

    dependencies added...

---
+++ newsletter/target/debug/deps/newsletter-92f1bc65fe4dd4a1.d
@@ -0,0 +1,5 @@
+/home/harry/zero2prod/newsletter/target/debug/deps/libnewsletter-92f1bc65fe4dd4a1.rmeta: src/main.rs
+
+/home/harry/zero2prod/newsletter/target/debug/deps/newsletter-92f1bc65fe4dd4a1.d: src/main.rs
+
+src/main.rs:
---
+++ newsletter/target/debug/deps/newsletter-98844f8c16ee300a.d
@@ -0,0 +1,5 @@
+/home/harry/zero2prod/newsletter/target/debug/deps/libnewsletter-98844f8c16ee300a.rmeta: src/main.rs
+
+/home/harry/zero2prod/newsletter/target/debug/deps/newsletter-98844f8c16ee300a.d: src/main.rs
+
+src/main.rs:
