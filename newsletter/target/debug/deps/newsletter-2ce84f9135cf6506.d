bee4d5022bdf84b6af3211b548a22580f65fc7fb
Xharry01X <harshitsingh50621@gmail.com>
2025-03-04 11:25:43 +0530

    file setup..

---
+++ newsletter/target/debug/deps/newsletter-2ce84f9135cf6506.d
@@ -0,0 +1,5 @@
+/home/harry/zero2prod/newsletter/target/debug/deps/libnewsletter-2ce84f9135cf6506.rmeta: src/main.rs
+
+/home/harry/zero2prod/newsletter/target/debug/deps/newsletter-2ce84f9135cf6506.d: src/main.rs
+
+src/main.rs:
---
+++ newsletter/target/debug/deps/newsletter-6ffd0663c361053e.d
@@ -0,0 +1,5 @@
+/home/harry/zero2prod/newsletter/target/debug/deps/libnewsletter-6ffd0663c361053e.rmeta: src/main.rs
+
+/home/harry/zero2prod/newsletter/target/debug/deps/newsletter-6ffd0663c361053e.d: src/main.rs
+
+src/main.rs:
